2a28fb68165fa43bf95378d343dee97632953dd0
Rob H <rob@extraservings.com>
2020-07-02 17:00:09 -0400

    working base commit

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const { Nuxt } = require('nuxt-start');
+
+const nuxtConfig = require('./nuxt.config.js');
+
+const config = {
+  ...nuxtConfig,
+  dev: false,
+  debug: false,
+  buildDir: 'nuxt',
+};
+const nuxt = new Nuxt(config);
+
+exports.ssrapp = functions.https.onRequest(async (req, res) => {
+  await nuxt.ready();
+  nuxt.render(req, res);
+});
